27fd3133f1cf95249d0ee7d38c8d3b1f0effb06a
Alexander Fiebig <alex.fiebig@online.de>
2018-05-19 18:48:30 +0200

    added new optimization plots + benchmark scripts now use max power measured

--- scripts/run_benchmark_ethminer.sh
+++ scripts/run_benchmark_ethminer.sh
@@ -4,11 +4,21 @@ device_id=$1
 mem_clock=$2
 graph_clock=$3
 
+bench_start=$(tail -n 1 power_results.txt | awk '{print $1}')
+
 ../../miner/windows/ethminer-0.9.41-genoil-1.1.7/ethminer.exe -U -M --cuda-devices $device_id > log_${mem_clock}_${graph_clock}.txt
 
-cat power_results.txt | tail -n 11 > power_results_${mem_clock}_${graph_clock}.txt
+if [[ -z $bench_start ]]
+then
+	cat power_results.txt > power_results_${mem_clock}_${graph_clock}.txt
+else
+	grep -A1000 "${bench_start}" power_results.txt > power_results_${mem_clock}_${graph_clock}.txt
+fi
+
 #bench_time=$(bc -l <<< "$(awk 'END {print $1}' power_results_${mem_clock}_${graph_clock}.txt)-$(awk 'NR==1 {print $1}' power_results_${mem_clock}_${graph_clock}.txt)")
-avg_power=$(awk 'NR<11{ total += $2 } END { print total/10 }' power_results_${mem_clock}_${graph_clock}.txt)
+#avg_power=$(awk '{ total += $2 } END { print total/NR }' power_results_${mem_clock}_${graph_clock}.txt)
+
+max_power=$(awk 'BEGIN{a=0}{if ($2>0+a) a=$2} END{print a}' power_results_${mem_clock}_${graph_clock}.txt)
 
 t1=$(grep 'Trial 1...' log_${mem_clock}_${graph_clock}.txt | sed 's/Trial [0-9]... //g')
 t2=$(grep 'Trial 2...' log_${mem_clock}_${graph_clock}.txt | sed 's/Trial [0-9]... //g')
@@ -16,8 +26,8 @@ t3=$(grep 'Trial 3...' log_${mem_clock}_${graph_clock}.txt | sed 's/Trial [0-9].
 t4=$(grep 'Trial 4...' log_${mem_clock}_${graph_clock}.txt | sed 's/Trial [0-9]... //g')
 t5=$(grep 'Trial 5...' log_${mem_clock}_${graph_clock}.txt | sed 's/Trial [0-9]... //g')
 avg_hashrate=$(./calc_inner_mean $t1 $t2 $t3 $t4 $t5)
-hashes_per_joule=$(awk "BEGIN { print ${avg_hashrate} / ${avg_power} }")
+hashes_per_joule=$(awk "BEGIN { print ${avg_hashrate} / ${max_power} }")
 
-echo ${mem_clock},${graph_clock},${avg_power},${avg_hashrate},${hashes_per_joule} >> result.dat
+echo ${mem_clock},${graph_clock},${max_power},${avg_hashrate},${hashes_per_joule} >> result.dat
 
 
--- scripts/run_benchmark_excavator.sh
+++ scripts/run_benchmark_excavator.sh
@@ -4,15 +4,25 @@ device_id=$1
 mem_clock=$2
 graph_clock=$3
 
+bench_start=$(tail -n 1 power_results.txt | awk '{print $1}')
+
 ../../miner/windows/excavator_v1.1.0a_Win64/excavator.exe -b -a equihash -cd $device_id &> log_${mem_clock}_${graph_clock}.txt
 
-cat power_results.txt | tail -n 11 > power_results_${mem_clock}_${graph_clock}.txt
+if [[ -z $bench_start ]]
+then
+	cat power_results.txt > power_results_${mem_clock}_${graph_clock}.txt
+else
+	grep -A1000 "${bench_start}" power_results.txt > power_results_${mem_clock}_${graph_clock}.txt
+fi
+
 #bench_time=$(bc -l <<< "$(awk 'END {print $1}' power_results_${mem_clock}_${graph_clock}.txt)-$(awk 'NR==1 {print $1}' power_results_${mem_clock}_${graph_clock}.txt)")
-avg_power=$(awk 'NR<11{ total += $2 } END { print total/10 }' power_results_${mem_clock}_${graph_clock}.txt)
+#avg_power=$(awk '{ total += $2 } END { print total/NR }' power_results_${mem_clock}_${graph_clock}.txt)
+
+max_power=$(awk 'BEGIN{a=0}{if ($2>0+a) a=$2} END{print a}' power_results_${mem_clock}_${graph_clock}.txt)
 
 avg_hashrate=$(grep -i 'Total measured:' log_${mem_clock}_${graph_clock}.txt | grep -Eo '[+-]?[0-9]+([.][0-9]+)?' | tail -n 1)
-hashes_per_joule=$(awk "BEGIN { print ${avg_hashrate} / ${avg_power} }")
+hashes_per_joule=$(awk "BEGIN { print ${avg_hashrate} / ${max_power} }")
 
-echo ${mem_clock},${graph_clock},${avg_power},${avg_hashrate},${hashes_per_joule} >> result.dat
+echo ${mem_clock},${graph_clock},${max_power},${avg_hashrate},${hashes_per_joule} >> result.dat
 
 
