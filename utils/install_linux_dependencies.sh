e0258c539e849debaaaa5ae30f64d18c5222b9e7
Alexander Fiebig <alex.fiebig@online.de>
2018-12-17 15:51:02 +0100

    update github branch

--- utils/install_linux_dependencies.sh
+++ utils/install_linux_dependencies.sh
@@ -1,12 +1,16 @@
 #!/bin/bash
 
-#debian
-sudo apt install libboost-all-dev libssl-dev libgoogle-glog-dev libeigen3-dev libcurl4-openssl-dev \
-libjansson-dev libmicrohttpd-dev libhwloc-dev libx11-dev automake autotools-dev build-essential cmake
-
-#suse
-#sudo zypper in boost-devel libopenssl-devel glog-devel eigen3-devel libcurl-devel \
-#libjansson-devel libmicrohttpd-devel hwloc-devel libX11-devel automake cmake
-
+if [ -x "$(command -v apt)" ]; then
+	#debian
+	sudo apt install libboost-all-dev libssl-dev libgoogle-glog-dev libeigen3-dev libcurl4-openssl-dev \
+	libjansson-dev libmicrohttpd-dev libhwloc-dev libx11-dev automake autotools-dev build-essential cmake
+elif [ -x "$(command -v zypper)" ]; then
+	#suse
+	sudo zypper in boost-devel libopenssl-devel glog-devel eigen3-devel libcurl-devel \
+	libjansson-devel libmicrohttpd-devel hwloc-devel libX11-devel automake cmake
+else
+	echo 'Error: Unknown package manager. Only apt (debian) and zypper (suse) supported.'
+	exit 1
+fi
 
 
--- utils/start_remote_X.sh
+++ utils/start_remote_X.sh
@@ -1,10 +1,22 @@
 #!/bin/bash
 
-#1)stop running x
-#suse
-#sudo rcxdm stop
-#ubuntu
-sudo service lightdm stop
+#1) stop running x
+if [ -x "$(command -v rcxdm)" ]; then
+	#suse
+	sudo rcxdm stop
+elif [ -x "$(command -v lightdm)" ]; then
+	#ubuntu 16.04
+	sudo service lightdm stop
+elif [ -x "$(command -v gdm3)" ]; then
+	#ubuntu 18.04
+	sudo service gdm3 stop
+else
+	echo 'Error: Unknown display manager. Stop X-Server manually.'
+	exit 1
+fi
 
-#start new x on remote display
+#2) start new x on remote display
 sudo nohup X -nolisten tcp :0 &
+
+#3) set display environment variable
+export DISPLAY=:0
